d51fc5214acf412ed3c1d12dc676d6b05af35e96
Darren Xu <darrenxu1994@gmail.com>
2022-09-02 07:48:25 +1000

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [// Enable Preact to support Preact JSX components.
-  preact(), // Enable React for the Algolia search component.
-  react(), vue(), mdx()],
-  site: `http://astro.build`
+  integrations: [
+    // Enable Preact to support Preact JSX components.
+    preact(), // Enable React for the Algolia search component.
+    react(),
+    vue(),
+    mdx(),
+  ],
+  site: `http://docs.darrenxu.com`,
 });
